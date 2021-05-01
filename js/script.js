93c583d48306c7450f42cf5a0dcfc6d63fb19a79
Your Name <natoli.missy@gmail.com>
2021-04-30 20:09:00 -0500

    added history buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var currentDay = (moment().format('(MM-DD-YYYY)'));
 console.log (currentDay);
+var historyButtons = document.querySelector("#history-btns");
+var cityButtons = document.querySelector(".history-btn");
+
 
 
 function showFuture() {
@@ -7,9 +10,15 @@ function showFuture() {
     futureDaysEl.removeAttribute("class", "hide");
 }
 
+
 function runProgram() {
     var cityName = document.getElementById('searchCity').value;
     console.log(cityName);
+    var newButton = document.createElement('button');
+    newButton.setAttribute('class', "history-btn");
+    newButton.textContent = cityName;
+    historyButtons.appendChild(newButton);
+
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=d80f9148da9f22ae1bde4d77fec27e30')
         .then(function(cityNameData) {
             return cityNameData.json();
@@ -21,7 +30,7 @@ function runProgram() {
             responseHeaderEl.innerHTML = '<h2>' + searchReponse + " " + currentDay + '<h2>';
             var latitude = cityNameData.coord.lat;
             var longitude = cityNameData.coord.lon;
-            return fetch ('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial' + '&appid=d80f9148da9f22ae1bde4d77fec27e30')
+            return fetch ('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial' + '&appid=d80f9148da9f22ae1bde4d77fec27e30');
         })
         .then(function(weatherResult) {
             return weatherResult.json();
@@ -67,6 +76,27 @@ function runProgram() {
             var dayTwoHumid = weatherResult.daily[1].humidity;
             var dayTwoEl = document.querySelector('#day-two');
             dayTwoEl.innerHTML = '<h4>' + twoDays + '<br>' + '<br>' + "Temp: " + dayTwoTemp + '<br>' + "Wind: " + dayTwoWind + " MPH" + '<br>' + "Humidity: " + dayTwoHumid + "%" + '<h4>';
+
+            var threeDays = (moment().add(3, 'days').format('MM-DD-YYYY'))
+            var dayThreeTemp = weatherResult.daily[2].temp.day;
+            var dayThreeWind = weatherResult.daily[2].wind_speed;
+            var dayThreeHumid = weatherResult.daily[2].humidity;
+            var dayThreeEl = document.querySelector('#day-three');
+            dayThreeEl.innerHTML = '<h4>' + threeDays + '<br>' + '<br>' + "Temp: " + dayThreeTemp + '<br>' + "Wind: " + dayThreeWind + " MPH" + '<br>' + "Humidity: " + dayThreeHumid + "%" + '<h4>';
+
+            var fourDays = (moment().add(4, 'days').format('MM-DD-YYYY'))
+            var dayFourTemp = weatherResult.daily[3].temp.day;
+            var dayFourWind = weatherResult.daily[3].wind_speed;
+            var dayFourHumid = weatherResult.daily[3].humidity;
+            var dayFourEl = document.querySelector('#day-four');
+            dayFourEl.innerHTML = '<h4>' + fourDays + '<br>' + '<br>' + "Temp: " + dayFourTemp + '<br>' + "Wind: " + dayFourWind + " MPH" + '<br>' + "Humidity: " + dayFourHumid + "%" + '<h4>';
+
+            var fiveDays = (moment().add(5, 'days').format('MM-DD-YYYY'))
+            var dayFiveTemp = weatherResult.daily[4].temp.day;
+            var dayFiveWind = weatherResult.daily[4].wind_speed;
+            var dayFiveHumid = weatherResult.daily[4].humidity;
+            var dayFiveEl = document.querySelector('#day-five');
+            dayFiveEl.innerHTML = '<h4>' + fiveDays + '<br>' + '<br>' + "Temp: " + dayFiveTemp + '<br>' + "Wind: " + dayFiveWind + " MPH" + '<br>' + "Humidity: " + dayFiveHumid + "%" + '<h4>';
         })
     showFuture();
 };
